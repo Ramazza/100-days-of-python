bc93bbf09dff9c9206896d200fb50e0c76a95a3e
Ramazza <86618328+Ramazza@users.noreply.github.com>
2023-01-27 11:57:06 -0300

    Delete Day 1 - Band Name Generator.py

--- Day 1 - Band Name Generator.py
+++
@@ -1,8 +0,0 @@
-print('Bem vindo ao Band Name Generator')
-cidade = input('Qual o nome da cidade que você nasceu?\n')
-pet = input('Qual o nome do seu pet?\n')
-print('O nome da sua banda pode ser ' + cidade + ' ' + pet + '.')
-
-
-
-
